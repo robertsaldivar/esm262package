0a6da954f974ead552d3e71ffc98d61f537bd93a
robertsaldivar <rnsaldivar@bren.ucsb.edu>
2020-03-11 17:00:37 -0700

    started creating a new function that will calculate the maximum and minimum temperatures of an input list.

---
+++ R/max_min_temp.R
@@ -0,0 +1,12 @@
+#' Max Min Temperature Function
+#'
+#' This function is intended to take a list of temperatures and return the max and min temperatures.
+#'
+#' @input The input is an a list of temperatures.
+#' @author Robert Saldivar
+#' @author Jennifer Truong
+#' @return Returns the maximum and minimum temperatures of the input list
+#'
+max_min_temp <- function(input) {
+
+}
