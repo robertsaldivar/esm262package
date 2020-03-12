893736b28886f71144e3dff49cdb569bd8a39e05
robertsaldivar <rnsaldivar@bren.ucsb.edu>
2020-03-11 18:29:31 -0700

    cleaned the weather data from noaa and created documentation for it

---
+++ R/noaa_weather_data.R
@@ -0,0 +1,7 @@
+#' Noaa Weather Data
+#'
+#' This is weather data from NOAA which include daily precipiation and daily temperatures for Santa Barbara
+#'
+#' @format is a table and data frame
+#' @source https://www.ncei.noaa.gov/metadata/geoportal/
+"noaa_weather_data"
