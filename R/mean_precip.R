bab07781874b55de09b00d45bea65440a951f817
jktruong <jktruong@bren.ucsb.edu>
2020-03-11 23:30:12 -0700

    Tried changing input to data frame, caused some errors

--- R/mean_precip.R
+++ R/mean_precip.R
@@ -3,13 +3,12 @@
 #' This function is going to calculate the mean of precipitation
 #'
 #'
-#' @param  a list of precipitation events
+#' @param  df data frame containing list of precipitation events
 #' @author Robert Saldivar
 #' @author Jennifer Truong
 #' @return the mean precipitation of the input precipitation events
 #'
-mean_precip <- function(input) {
-mean = input/length(input)
-
-return(mean)
+mean_precip <- function(df) {
+  meanprecip = apply(df, 2, mean)
+  return(meanprecip)
 }
