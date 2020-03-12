a4153c686f72fd0ed139217cc43d21336bf1bccd
jktruong <jktruong@bren.ucsb.edu>
2020-03-11 17:17:25 -0700

    Started writing documentation for water supply function

---
+++ R/water_supply.R
@@ -0,0 +1,8 @@
+#' Water Supply
+#'
+#' This function calculates water supply based on precipitation trends
+#'
+#' @param input
+#' @author Robert Saldivar
+#' @author Jennifer Truong
+#' @return Amount of water supply (in kg/m^3)
