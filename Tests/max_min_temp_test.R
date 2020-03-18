c150fefdf4bf31ea4ed5fa48fe05e57de0fd23bc
robertsaldivar <rnsaldivar@bren.ucsb.edu>
2020-03-18 15:26:54 -0700

    added some notes about the max min temp test

--- Tests/max_min_temp_test.R
+++ Tests/max_min_temp_test.R
@@ -1,5 +1,11 @@
 #min_man_temp_test
 
+#These tests are intended to make sure that the function is working logically.
+
+#The Maximum temperature is greater than the minimum temperature.
+#The Maximum temperature is within a reasonable range.
+#The Minimum temperature is within a reasonable range.
+
 test_that("Max_Min_Temp_works",
           {
             expect_true(max_min_temp(noaa_weather_data)$max > max_min_temp(noaa_weather_data)$min)
