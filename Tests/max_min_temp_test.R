b1d88a6483ad97a92a94f76dfa207f95df5e1d3a
robertsaldivar <rnsaldivar@bren.ucsb.edu>
2020-03-18 15:08:20 -0700

    created a test for the max min temp function

--- Tests/max_min_temp_test.R
+++ Tests/max_min_temp_test.R
@@ -1,1 +1,9 @@
-min_man_temp_test
+#min_man_temp_test
+
+test_that("Max_Min_Temp_works",
+          {
+            expect_true(max_min_temp(noaa_weather_data)$max > max_min_temp(noaa_weather_data)$min)
+            expect_true(max_min_temp(noaa_weather_data)$max < 250)
+            expect_true(max_min_temp(noaa_weather_data)$min > -150)
+          })
+
